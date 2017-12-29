9540b74cc3c4b6f05070ada906c4bf0672ccf7ea
sunakshi96 <sunakshitejwani@gmail.com>
2017-12-29 21:25:23 +0530

    count for non - existing words

--- App1_InteractiveDictionary/app1.py
+++ App1_InteractiveDictionary/app1.py
@@ -2,7 +2,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(word):
-	return data[word]
+	if word in data:
+		return data[word]
+	else:
+		return "The word does n't exist. Please double check it."
+	
 
 word = input("Enter word:")
 print(translate(word))
