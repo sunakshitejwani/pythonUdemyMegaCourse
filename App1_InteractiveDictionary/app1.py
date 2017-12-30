062447be9264c5418d5fccfb8686e8a526106023
sunakshi96 <sunakshitejwani@gmail.com>
2017-12-30 16:45:36 +0530

    handle the Proper Nouns

--- App1_InteractiveDictionary/app1.py
+++ App1_InteractiveDictionary/app1.py
@@ -8,6 +8,8 @@ def translate(word):
 	word = word.lower()
 	if word in data:
 		return data[word]
+	elif word.title() in data: #if user entered "texas" this will check for "Texas" as well
+		return data[word.title()]
 	elif len(get_close_matches(word,data.keys()))>0:
 		yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word,data.keys())[0])
 		if yn=="Y":
@@ -22,7 +24,7 @@ def translate(word):
 	
 
 word = input("Enter word:")
-output = +translate(word)
+output = translate(word)
 if type(output) == list:
 	for item in output:
 		print (item)
